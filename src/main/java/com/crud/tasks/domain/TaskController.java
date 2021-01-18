e27d839387b72fd27c515ca1fdc0c8ca48647ebe
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-01-18 21:56:33 +0100

    19.2 DbService, TaskRepository, TaskMapper added. build.gradle mysql dependencies added, application.properties database credentials

--- src/main/java/com/crud/tasks/domain/TaskController.java
+++ src/main/java/com/crud/tasks/domain/TaskController.java
@@ -1,17 +1,24 @@
 package com.crud.tasks.domain;
 
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(value = "getTask")
