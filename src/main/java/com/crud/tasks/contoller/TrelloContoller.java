d387c2d16cf84b6baf2ec0bd52e74da97786c895
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-02-04 15:50:09 +0100

    22.2 Course materials added and exercise created.

---
+++ src/main/java/com/crud/tasks/contoller/TrelloContoller.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.contoller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+@RequiredArgsConstructor
+public class TrelloContoller {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream().filter(trelloBoardDto -> trelloBoardDto.getName() != null && trelloBoardDto.getId() != null
+                            && trelloBoardDto.getName().contains("Kodilla"))
+                            .forEach(trelloBoardDto -> {
+                                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                            });
+    }
+
+}
