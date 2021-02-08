d8991d42f418cb544a18e112044b8b185b5fc6b5
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-02-08 17:41:48 +0100

    22.4 Course materials and  exercise - test shouldReturnEmptyList() added

--- src/main/java/com/crud/tasks/contoller/TrelloContoller.java
+++ src/main/java/com/crud/tasks/contoller/TrelloContoller.java
@@ -17,19 +17,8 @@ public class TrelloContoller {
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.stream().filter(trelloBoardDto -> trelloBoardDto.getName() != null && trelloBoardDto.getId() != null
-                            && trelloBoardDto.getName().contains("Kodilla"))
-                            .forEach(trelloBoardDto -> {
-                                System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-                                System.out.println("This board contains lists: ");
-                                trelloBoardDto.getLists().forEach(trelloList -> {
-                                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-                                });
-                            });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @PostMapping("createTrelloCard")
