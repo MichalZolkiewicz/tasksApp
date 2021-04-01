5e578baed910146150df6c88ab0a64d601a1102a
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-01 16:58:13 +0200

    29.3 Course materials

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
-public class TrelloContoller {
+public class TrelloController {
 
     @Autowired
     private final TrelloFacade trelloFacade;
