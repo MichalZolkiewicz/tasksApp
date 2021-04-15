260021170ec6f221701f0880a06735875e496b67
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-15 22:34:00 +0200

    32.3 Template showing numbers of cards in database created.

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,14 +1,21 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private TaskRepository taskRepository;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -18,17 +25,33 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("company_name", adminConfig.getCompanyName());
-        context.setVariable("company_goal", adminConfig.getCompanyGoal());
-        context.setVariable("company_mail", adminConfig.getCompanyMail());
-        context.setVariable("company_phone", adminConfig.getCompanyPhone());
         context.setVariable("thank_you", "Thank you,");
         context.setVariable("kind_regards", "Kind regards,");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String buildEmailShowingCardsInDatabases(String message) {
+        Context context = new Context();
+        context.setVariable("message", "This is number of cards in you database: ");
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("task_repository", taskRepository);
+        context.setVariable("thank_you", "Thank you,");
+        context.setVariable("kind_regards", "Kind regards,");
+        context.setVariable("show_button", false);
+        return templateEngine.process("mail/cards-in-database-once-a-day", context);
     }
 }
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -36,7 +36,7 @@ public class SimpleEmailService {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
-            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+            messageHelper.setText(mailCreatorService.buildEmailShowingCardsInDatabases(mail.getMessage()), true);
         };
     }
 
@@ -44,8 +44,17 @@ public class SimpleEmailService {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        mailMessage.setText(mailCreatorService.buildEmailShowingCardsInDatabases(mail.getMessage()));
 
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessageOnceADay(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildEmailShowingCardsInDatabases(mail.getMessage()), true);
+        };
     }
 }
