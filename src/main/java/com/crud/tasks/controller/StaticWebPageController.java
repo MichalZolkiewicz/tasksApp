4fe3af8fb165dfefc13da2b18ea08bf10cec0103
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-04-16 00:35:21 +0200

    heroku

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,14 +1,15 @@
 package com.crud.tasks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class StaticWebPageController {
 
-    @RequestMapping("/")
     public String index(Map<String, Object> model) {
         model.put("variable", "My Thymeleaf variable");
         model.put("one", 1);
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -9,6 +9,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
